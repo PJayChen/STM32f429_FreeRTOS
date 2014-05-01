879587dbe6a6b9c7c66ebedaaca3e9fb705dd29e
PJayChen <n26021539@mail.ncku.edu.tw>
2014-04-30 20:42:55 -0700

    Priority test Higher priority task reset the PA9/10 then suspend 5 ms Lower priority task set the PA9/10

--- hw_conf.c
+++ hw_conf.c
@@ -5,15 +5,13 @@ void GPIO_Configuration(void)
 {
 	GPIO_InitTypeDef GPIO_InitStructure;
 
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9 | GPIO_Pin_10;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
         GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
 	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_DOWN;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
         GPIO_Init(GPIOA, &GPIO_InitStructure);
 
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1;
-	GPIO_Init(GPIOA, &GPIO_InitStructure);
 }
 
 
--- main.c
+++ main.c
@@ -4,15 +4,15 @@
 void vATask(void *pvParameters)
 {
 	while(1){
-		GPIOA->ODR = 0x00000001;
-		vTaskDelay(1000);
+		GPIOA->ODR = 0x00000000;
+		vTaskDelay(5/portTICK_PERIOD_MS);
 	}
 }
 
 void vBTask(void *pvParameters)
 {
 	while(1){
-		GPIOA->ODR = 0x00000002;
+		GPIOA->ODR = 0x00000f00;
 	}
 }
 
@@ -20,7 +20,7 @@ int main(void)
 {
 	
 	prvSetupHardware();
-	
+	GPIOA->ODR = 0x00000f00;
 	xTaskCreate(vATask, "A", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, NULL);
 	xTaskCreate(vBTask, "B", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY, NULL);
 
